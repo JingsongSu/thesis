bea6e8c706b1cc4341f8041ac3227f14189492c1
JingsongSu <2608325709@qq.com>
2026-07-20 17:27:02 +0800

    更新配置

--- HoloRec-TIGER/run_test.sh
+++ HoloRec-TIGER/run_test.sh
@@ -1,6 +1,6 @@
 DATASET=Instruments
 DATA_PATH=../data
-CKPT_PATH=./ckpt/$DATASET-ceshi/checkpoint-16254
+CKPT_PATH=./ckpt/$DATASET-base/checkpoint-18447
 
 RESULTS_FILE=./results/$DATASET/all-interleave.json
 SAVE_PRED_TXT=/home/jovyan/ceph-1/sujinsong/sujinsong/thesis/LETTER-master/LETTER-TIGER-new/predictions.txt
@@ -17,4 +17,4 @@ torchrun --nproc_per_node=8 --master_port=2214 test.py \
   --save_pred_txt $SAVE_PRED_TXT \
   --test_prompt_ids 0 \
   --index_file .tw32.json \
-  --coarse_index_file .tw8.json
+  --coarse_index_file .tw32.json
--- HoloRec-TIGER/run_train.sh
+++ HoloRec-TIGER/run_train.sh
@@ -3,17 +3,17 @@ export WANDB_MODE=disabled
 export CUDA_VISIBLE_DEVICES=0,1
 
 DATASET=Instruments
-OUTPUT_DIR=./ckpt/$DATASET-ceshi/
+OUTPUT_DIR=./ckpt/$DATASET-base/
 
 torchrun --nproc_per_node=2 --master_port=4314 ./finetune.py \
     --output_dir $OUTPUT_DIR \
     --dataset $DATASET \
     --per_device_batch_size 256 \
     --learning_rate 5e-4 \
-    --epochs 400 \
+    --epochs 200 \
     --index_file .tw32.json \
-    --coarse_index_file .tw8.json \
+    --coarse_index_file .tw32.json \
     --temperature 0.7 \
-    --coarse_loss_weight 0.5 \
+    --coarse_loss_weight 5 \
     --fine_loss_weight 1.0 \
-    --coarse_align_weight 10.0
+    --coarse_align_weight 200.0

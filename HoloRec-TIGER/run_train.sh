306b363eacc3b779eb3881791406870ace40445c
JingsongSu <2608325709@qq.com>
2026-07-21 10:26:10 +0800

    参数更新

--- HoloRec-TIGER/run_train.sh
+++ HoloRec-TIGER/run_train.sh
@@ -1,19 +1,19 @@
 export WANDB_MODE=disabled
 # export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=2,3
 
-DATASET=Instruments
-OUTPUT_DIR=./ckpt/$DATASET-base/
+DATASET=Beauty
+OUTPUT_DIR=./ckpt/$DATASET-fuxian-tw32tw8/
 
-torchrun --nproc_per_node=2 --master_port=4314 ./finetune.py \
+torchrun --nproc_per_node=2 --master_port=4311 ./finetune.py \
     --output_dir $OUTPUT_DIR \
     --dataset $DATASET \
     --per_device_batch_size 256 \
     --learning_rate 5e-4 \
-    --epochs 200 \
+    --epochs 400 \
     --index_file .tw32.json \
-    --coarse_index_file .tw32.json \
-    --temperature 0.7 \
-    --coarse_loss_weight 5 \
+    --coarse_index_file .tw8.json \
+    --temperature 1.0 \
+    --coarse_loss_weight 0.5 \
     --fine_loss_weight 1.0 \
-    --coarse_align_weight 200.0
+    --coarse_align_weight 30.0
